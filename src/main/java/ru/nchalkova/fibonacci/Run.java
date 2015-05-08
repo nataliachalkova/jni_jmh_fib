8400de62a325255315f04b5dd8bded7f62e0ffc0
nataliachalkova <natalia.chalkova@gmail.com>
2015-05-08 17:23:26 +0300

    fix path string

--- src/main/java/ru/nchalkova/fibonacci/Run.java
+++ src/main/java/ru/nchalkova/fibonacci/Run.java
@@ -1,14 +1,18 @@
 package ru.nchalkova.fibonacci;
+
+import java.nio.file.Paths;
+
 /**
- * Run calculating Fn fibonacci for n in java and native code
+ * Run calculating Fn fibonacci number for n in java and native code
  * */
 public class Run {
 	static {
-		System.load(System.getProperty("user.dir")+"/target/libfibonacci.dylib");
+		System.load(Paths.get(System.getProperty("user.dir"), "target", "libfibonacci.dylib").toString());
 	}
+
 	public static void main(String[] args) {
 		FibonacciFacade f = new FibonacciFacade();
-		
+
 		System.out.println(f.invokeJavaFib(8));
 		System.out.println(f.invokeNativeFib(8));
 	}
